7a650ff69505e32321d429f07d4873192b31e8cf
jefkruz <josephugbeva@gmail.com>
2023-07-04 08:01:45 +0100

    added mobile menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -113,192 +113,26 @@ $contact_menu = (isset($contact_menu) && $contact_menu == true) ? 'active' : '';
     <div class="sidenav-content">
         <!-- Sidebar  -->
         <nav id="sidebar" class="sidenav">
-            <h2 id="web-name"><span><i class="fa fa-plane"></i></span>Star Travel</h2>
+            <h2 id="web-name"><span><i class="fa fa-plane"></i></span>Air Flourish</h2>
 
             <div id="main-menu">
                 <div id="dismiss">
                     <button class="btn" id="closebtn">&times;</button>
                 </div>
                 <div class="list-group panel">
-                    <a href="#home-links" class="items-list active" data-toggle="collapse" aria-expanded="false">
-                        <span><i class="fa fa-home link-icon"></i></span>Home<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu text-danger" id="home-links">
-                        <a class="items-list active" href="index.html">Main Homepage</a>
-                        <a class="items-list" href="flight-homepage.html">Flight Homepage</a>
-                        <a class="items-list" href="hotel-homepage.html">Hotel Homepage</a>
-                        <a class="items-list" href="tour-homepage.html">Tour Homepage</a>
-                        <a class="items-list" href="cruise-homepage.html">Cruise Homepage</a>
-                        <a class="items-list" href="car-homepage.html">Car Homepage</a>
-                        <a class="items-list" href="landing-page.html">Landing Page</a>
-                        <a class="items-list" href="travel-agency-homepage.html">Travel Agency Page</a>
-                    </div><!-- end sub-menu -->
+                    <a href="{{route('home')}}" class="items-list {{$home_menu}}" data-toggle="collapse" aria-expanded="false">
+                        <span><i class="fa fa-home link-icon"></i></span>Home<span></span></a>
 
-                    <a class="items-list" href="#flights-links" data-toggle="collapse"><span><i class="fa fa-plane link-icon"></i></span>Flights<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu" id="flights-links">
-                        <a class="items-list" href="flight-homepage.html">Flight Homepage</a>
-                        <a class="items-list" href="flight-listing-left-sidebar.html">List View Left Sidebar</a>
-                        <a class="items-list" href="flight-listing-right-sidebar.html">List View Right Sidebar</a>
-                        <a class="items-list" href="flight-grid-left-sidebar.html">Grid View Left Sidebar</a>
-                        <a class="items-list" href="flight-grid-right-sidebar.html">Grid View Right Sidebar</a>
-                        <a class="items-list" href="flight-detail-left-sidebar.html">Detail Left Sidebar</a>
-                        <a class="items-list" href="flight-detail-right-sidebar.html">Detail Right Sidebar</a>
-                        <a class="items-list" href="flight-booking-left-sidebar.html">Booking Left Sidebar</a>
-                        <a class="items-list" href="flight-booking-right-sidebar.html">Booking Right Sidebar</a>
-                        <a class="items-list" href="flight-search-result.html">Search Result</a>
-                        <a class="items-list" href="flight-offers.html">Hot Offers</a>
-                    </div><!-- end sub-menu -->
 
-                    <a class="items-list" href="#hotels-links" data-toggle="collapse"><span><i class="fa fa-building link-icon"></i></span>Hotels<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu" id="hotels-links">
-                        <a class="items-list" href="hotel-homepage.html">Hotel Homepage</a>
-                        <a class="items-list" href="hotel-listing-left-sidebar.html">List View Left Sidebar</a>
-                        <a class="items-list" href="hotel-listing-right-sidebar.html">List View Right Sidebar</a>
-                        <a class="items-list" href="hotel-grid-left-sidebar.html">Grid View Left Sidebar</a>
-                        <a class="items-list" href="hotel-grid-right-sidebar.html">Grid View Right Sidebar</a>
-                        <a class="items-list" href="hotel-detail-left-sidebar.html">Detail Left Sidebar</a>
-                        <a class="items-list" href="hotel-detail-right-sidebar.html">Detail Right Sidebar</a>
-                        <a class="items-list" href="hotel-booking-left-sidebar.html">Booking Left Sidebar</a>
-                        <a class="items-list" href="hotel-booking-right-sidebar.html">Booking Right Sidebar</a>
-                        <a class="items-list" href="hotel-search-result.html">Search Result</a>
-                        <a class="items-list" href="hotel-offers.html">Hot Offers</a>
-                    </div><!-- end sub-menu -->
+                    <a class="items-list {{$hotel_menu}}" href="{{route('hotels')}}" ><span><i class="fa fa-building link-icon"></i></span>Hotels</a>
 
-                    <a class="items-list" href="#tours-links" data-toggle="collapse"><span><i class="fa fa-globe link-icon"></i></span>Tours<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu" id="tours-links">
-                        <a class="items-list" href="tour-homepage.html">Tour Homepage</a>
-                        <a class="items-list" href="tour-listing-left-sidebar.html">List View Left Sidebar</a>
-                        <a class="items-list" href="tour-listing-right-sidebar.html">List View Right Sidebar</a>
-                        <a class="items-list" href="tour-grid-left-sidebar.html">Grid View Left Sidebar</a>
-                        <a class="items-list" href="tour-grid-right-sidebar.html">Grid View Right Sidebar</a>
-                        <a class="items-list" href="tour-detail-left-sidebar.html">Detail Left Sidebar</a>
-                        <a class="items-list" href="tour-detail-right-sidebar.html">Detail Right Sidebar</a>
-                        <a class="items-list" href="tour-booking-left-sidebar.html">Booking Left Sidebar</a>
-                        <a class="items-list" href="tour-booking-right-sidebar.html">Booking Right Sidebar</a>
-                        <a class="items-list" href="tour-search-result.html">Search Result</a>
-                        <a class="items-list" href="tour-offers.html">Hot Offers</a>
-                    </div><!-- end sub-menu -->
+                    <a class="items-list {{$tour_menu}}" href="{{route('tours')}}" ><span><i class="fa fa-globe link-icon"></i></span>Tours<span></a>
 
-                    <a class="items-list" href="#cruise-links" data-toggle="collapse"><span><i class="fa fa-ship link-icon"></i></span>Cruise<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu" id="cruise-links">
-                        <a class="items-list" href="cruise-homepage.html">Cruise Homepage</a>
-                        <a class="items-list" href="cruise-listing-left-sidebar.html">List View Left Sidebar</a>
-                        <a class="items-list" href="cruise-listing-right-sidebar.html">List View Right Sidebar</a>
-                        <a class="items-list" href="cruise-grid-left-sidebar.html">Grid View Left Sidebar</a>
-                        <a class="items-list" href="cruise-grid-right-sidebar.html">Grid View Right Sidebar</a>
-                        <a class="items-list" href="cruise-detail-left-sidebar.html">Detail Left Sidebar</a>
-                        <a class="items-list" href="cruise-detail-right-sidebar.html">Detail Right Sidebar</a>
-                        <a class="items-list" href="cruise-booking-left-sidebar.html">Booking Left Sidebar</a>
-                        <a class="items-list" href="cruise-booking-right-sidebar.html">Booking Right Sidebar</a>
-                        <a class="items-list" href="cruise-search-result.html">Search Result</a>
-                        <a class="items-list" href="cruise-offers.html">Hot Offers</a>
-                    </div><!-- end sub-menu -->
 
-                    <a class="items-list" href="#cars-links" data-toggle="collapse"><span><i class="fa fa-car link-icon"></i></span>Cars<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu" id="cars-links">
-                        <a class="items-list" href="car-homepage.html">Car Homepage</a>
-                        <a class="items-list" href="car-listing-left-sidebar.html">List View Left Sidebar</a>
-                        <a class="items-list" href="car-listing-right-sidebar.html">List View Right Sidebar</a>
-                        <a class="items-list" href="car-grid-left-sidebar.html">Grid View Left Sidebar</a>
-                        <a class="items-list" href="car-grid-right-sidebar.html">Grid View Right Sidebar</a>
-                        <a class="items-list" href="car-detail-left-sidebar.html">Detail Left Sidebar</a>
-                        <a class="items-list" href="car-detail-right-sidebar.html">Detail Right Sidebar</a>
-                        <a class="items-list" href="car-booking-left-sidebar.html">Booking Left Sidebar</a>
-                        <a class="items-list" href="car-booking-right-sidebar.html">Booking Right Sidebar</a>
-                        <a class="items-list" href="car-search-result.html">Search Result</a>
-                        <a class="items-list" href="car-offers.html">Hot Offers</a>
-                    </div><!-- end sub-menu -->
+                    <a class="items-list {{$blog_menu}}" href="{{route('news')}}" ><span><i class="fa fa-newspaper-o link-icon"></i></span>News</a>
 
-                    <a class="items-list" href="#features-links" data-toggle="collapse"><span><i class="fa fa-puzzle-piece link-icon"></i></span>Features<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu mega-sub-menu" id="features-links">
-                        <a class="items-list" href="#header-style-links" data-toggle="collapse">Header<span><i class="fa fa-caret-down arrow"></i></span></a>
-                        <div class="collapse sub-menu mega-sub-menu-links" id="header-style-links">
-                            <a class="items-list" href="feature-header-style-1.html">Header Style 1</a>
-                            <a class="items-list" href="feature-header-style-2.html">Header Style 2</a>
-                            <a class="items-list" href="feature-header-style-3.html">Header Style 3</a>
-                            <a class="items-list" href="feature-header-style-4.html">Header Style 4</a>
-                            <a class="items-list" href="feature-header-style-5.html">Header Style 5</a>
-                            <a class="items-list" href="feature-header-style-6.html">Header Style 6</a>
-                        </div>
-                        <a class="items-list" href="#page-title-style-links" data-toggle="collapse">Page Title<span><i class="fa fa-caret-down arrow"></i></span></a>
-                        <div class="collapse sub-menu mega-sub-menu-links" id="page-title-style-links">
-                            <a class="items-list" href="feature-page-title-style-1.html">Page Title Style 1</a>
-                            <a class="items-list" href="feature-page-title-style-2.html">Page Title Style 2</a>
-                            <a class="items-list" href="feature-page-title-style-3.html">Page Title Style 3</a>
-                            <a class="items-list" href="feature-page-title-style-4.html">Page Title Style 4</a>
-                            <a class="items-list" href="feature-page-title-style-5.html">Page Title Style 5</a>
-                            <a class="items-list" href="feature-page-title-style-6.html">Page Title Style 6</a>
-                        </div>
-                        <a class="items-list" href="#footer-style-links" data-toggle="collapse">Footer<span><i class="fa fa-caret-down arrow"></i></span></a>
-                        <div class="collapse sub-menu mega-sub-menu-links" id="footer-style-links">
-                            <a class="items-list" href="feature-footer-style-1.html">Footer Style 1</a>
-                            <a class="items-list" href="feature-footer-style-2.html">Footer Style 2</a>
-                            <a class="items-list" href="feature-footer-style-3.html">Footer Style 3</a>
-                            <a class="items-list" href="feature-footer-style-4.html">Footer Style 4</a>
-                            <a class="items-list" href="feature-footer-style-5.html">Footer Style 5</a>
-                        </div>
-                        <a class="items-list" href="#f-blog-links" data-toggle="collapse">Blog<span><i class="fa fa-caret-down arrow"></i></span></a>
-                        <div class="collapse sub-menu mega-sub-menu-links" id="f-blog-links">
-                            <a class="items-list" href="blog-listing-left-sidebar.html">Blog Listing Left Sidebar</a>
-                            <a class="items-list" href="blog-listing-right-sidebar.html">Blog Listing Right Sidebar</a>
-                            <a class="items-list" href="blog-detail-left-sidebar.html">Blog Detail Left Sidebar</a>
-                            <a class="items-list" href="blog-detail-right-sidebar.html">Blog Detail Right Sidebar</a>
-                        </div>
-                        <a class="items-list" href="#f-gallery-links" data-toggle="collapse">Gallery<span><i class="fa fa-caret-down arrow"></i></span></a>
-                        <div class="collapse sub-menu mega-sub-menu-links" id="f-gallery-links">
-                            <a class="items-list" href="gallery-masonry.html">Gallery Masonry</a>
-                            <a class="items-list" href="gallery-2-columns.html">Gallery 2 Columns</a>
-                            <a class="items-list" href="gallery-3-columns.html">Gallery 3 Columns</a>
-                            <a class="items-list" href="gallery-4-columns.html">Gallery 4 Columns</a>
-                        </div>
-                        <a class="items-list" href="#f-forms-links" data-toggle="collapse">Forms<span><i class="fa fa-caret-down arrow"></i></span></a>
-                        <div class="collapse sub-menu mega-sub-menu-links" id="f-forms-links">
-                            <a class="items-list" href="login-1.html">Login 1</a>
-                            <a class="items-list" href="login-2.html">Login 2</a>
-                            <a class="items-list" href="registration-1.html">Registration 1</a>
-                            <a class="items-list" href="registration-2.html">Registration 2</a>
-                            <a class="items-list" href="forgot-password-1.html">Forgot Password 1</a>
-                            <a class="items-list" href="forgot-password-2.html">Forgot Password 2</a>
-                        </div>
-                    </div><!-- end sub-menu -->
+                    <a class="items-list {{$contact_menu}}" href="{{route('contact-us')}}" ><span><i class="fa fa-puzzle-piece link-icon"></i></span>Contact Us</a>
 
-                    <a class="items-list" href="#pages-links" data-toggle="collapse"><span><i class="fa fa-clone link-icon"></i></span>Pages<span><i class="fa fa-chevron-down arrow"></i></span></a>
-                    <div class="collapse sub-menu" id="pages-links">
-                        <div class="list-group-heading ">Standard <span>Pages</span></div>
-                        <a class="items-list" href="about-us-1.html">About Us 1</a>
-                        <a class="items-list" href="about-us-2.html">About Us 2</a>
-                        <a class="items-list" href="services-1.html">Services 1</a>
-                        <a class="items-list" href="services-2.html">Services 2</a>
-                        <a class="items-list" href="team-1.html">Our Team 1</a>
-                        <a class="items-list" href="team-2.html">Our Team 2</a>
-                        <a class="items-list" href="contact-us-1.html">Contact Us 1</a>
-                        <a class="items-list" href="contact-us-2.html">Contact Us 2</a>
-                        <div class="list-group-heading ">User <span>Dashboard</span></div>
-                        <a class="items-list" href="dashboard-1.html">Dashboard 1</a>
-                        <a class="items-list" href="dashboard-2.html">Dashboard 2</a>
-                        <a class="items-list" href="user-profile.html">User Profile</a>
-                        <a class="items-list" href="booking.html">Booking</a>
-                        <a class="items-list" href="wishlist.html">Wishlist</a>
-                        <a class="items-list" href="cards.html">Cards</a>
-                        <div class="list-group-heading ">Special <span>Pages</span></div>
-                        <a class="items-list" href="error-page-1.html">404 Page 1</a>
-                        <a class="items-list" href="error-page-2.html">404 Page 2</a>
-                        <a class="items-list" href="coming-soon-1.html">Coming Soon 1</a>
-                        <a class="items-list" href="coming-soon-2.html">Coming Soon 2</a>
-                        <a class="items-list" href="faq-left-sidebar.html">FAQ Left Sidebar</a>
-                        <a class="items-list" href="faq-right-sidebar.html">FAQ Right Sidebar</a>
-                        <a class="items-list" href="testimonials-1.html">Testimonials 1</a>
-                        <a class="items-list" href="testimonials-2.html">Testimonials 2</a>
-                        <div class="list-group-heading ">Extra <span>Pages</span></div>
-                        <a class="items-list" href="before-you-fly.html">Before Fly</a>
-                        <a class="items-list" href="travel-insurance.html">Travel Insurance</a>
-                        <a class="items-list" href="travel-guide.html">Travel Guide</a>
-                        <a class="items-list" href="holidays.html">Holidays</a>
-                        <a class="items-list" href="thank-you.html">Thank You</a>
-                        <a class="items-list" href="payment-success.html">Payment Success</a>
-                        <a class="items-list" href="pricing-table-1.html">Pricing Table 1</a>
-                        <a class="items-list" href="pricing-table-2.html">Pricing Table 2</a>
-                        <a class="items-list" href="popup-ad.html">Popup Ad</a>
-                    </div><!-- end sub-menu -->
 
                 </div><!-- End list-group panel -->
             </div><!-- End main-menu -->
